cb2eb6d2c2beba5de304b79d6ab907dd75ac536a
Faical Zourgane <faical_zourgane@um5.ac.ma>
2025-04-02 01:32:01 +0000

    Message décrivant tes modifications

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         FAICAL ZOURGANE
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        ZF
       </h1>
     </div>
   );
