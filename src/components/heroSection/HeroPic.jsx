f9c1399c715545e2e5ba3fae38fb649421565586
Faical Zourgane <faical_zourgane@um5.ac.ma>
2025-04-02 01:12:22 +0000

    Message décrivant tes modifications

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/images/me2.png"
+        src="/portfolio/images/me2.png"
         alt="ZOURGANE FAICAL"
         className="max-h-[450px] w-auto"
       />
